83f4c3251c530f5406f85b8f6954319befaadfe1
hiropu0824 <hiropu0824@gmail.com>
2024-02-11 03:28:27 +0900

    [Update] クラス受け渡しに対するアプローチの変更、ベースクラスを利用した設計方針で行う。

--- Assets/MyGame/Scripts/BaseSystem/ResourceManager.cs
+++ Assets/MyGame/Scripts/BaseSystem/ResourceManager.cs
@@ -3,35 +3,14 @@ using UniRx;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
-/// <summary>
-///     現在のリソースの秒間生産力を提供してほしい
-/// </summary>
-public interface IFacilityPowerProvider
-{
-    ReactiveProperty<float> CurrentFacilityPower { get;}
-}
-/// <summary>
-/// 現在の1クリック生産力を提供してほしい
-/// </summary>
-public interface IClickPowerProvider
-{
-    ReactiveProperty<float> CurrentClickPower { get; }
-}
+
 public class ResourceManager : SingletonMonoBehavior<ResourceManager>
 {
-    /// <summary>
-    /// ここでIFacilityPowerProvider、IClickPowerProvider型でコンポーネントを受け取りたい。
-    /// </summary>
-    [SerializeField , Header("生産力を実装するクラス")] private Object _testPowerProvider;
-    private IFacilityPowerProvider _facilityPowerProvider => _testPowerProvider as IFacilityPowerProvider;
-    private IClickPowerProvider _clickPowerProvider=> _testPowerProvider as IClickPowerProvider;
-    
+    [SerializeField , Header("生産力を実装するクラス")] private PowerProviderBase _powerProvider　;
     
     private decimal _currentResources;
     private float _currentFacilityPower;
     private float _currentClickPower;
-
-
 
     /// <summary>
     ///     現在のリソース値が変更された場合に呼ばれる
@@ -96,8 +75,8 @@ public class ResourceManager : SingletonMonoBehavior<ResourceManager>
 
     protected override void OnAwake()
     {
-        _facilityPowerProvider.CurrentFacilityPower.Subscribe(x => CurrentFacilityPower = x).AddTo(this);
-        _clickPowerProvider.CurrentClickPower.Subscribe(x => CurrentClickPower = x).AddTo(this);
+        _powerProvider.CurrentFacilityPower.Subscribe(x => CurrentFacilityPower = x).AddTo(this);
+        _powerProvider.CurrentClickPower.Subscribe(x => CurrentClickPower = x).AddTo(this);
     }
 
     /// <summary>
--- Assets/MyGame/Scripts/Test/TestPowerProvider.cs
+++ Assets/MyGame/Scripts/Test/TestPowerProvider.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 
-public class TestPowerProvider : MonoBehaviour , IFacilityPowerProvider , IClickPowerProvider
+public class TestPowerProvider : PowerProviderBase
 {
     [SerializeField, Header("テスト用秒間資源生成量")]  private float _testFacilityPower;
     [SerializeField, Header("テスト用秒間1クリック生成量")]  private float _testClickPower;
 
-    public ReactiveProperty<float> CurrentFacilityPower { get; } = new();
-    public ReactiveProperty<float> CurrentClickPower { get; } = new();
+    public override ReactiveProperty<float> CurrentFacilityPower { get; } = new();
+    public override ReactiveProperty<float> CurrentClickPower { get; } = new();
 
     private void FixedUpdate()
     {
