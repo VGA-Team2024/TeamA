50b7dd65a42e40a5e30db946da39b681f2256647
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-02-11 18:30:44 +0900

    [add]クリックするオブジェクトの作成

---
+++ Assets/MyGame/Scripts/BaseSystem/ClickEvent.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// クッキー(仮)をクリックしたときの処理
+/// </summary>
+public class ClickEvent : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (ResourceManager.Instance != null)
+        {
+            ResourceManager.Instance.OnClick();
+        }
+        else
+        {
+            Debug.Log("ResourceManagerがありません");
+        }
+    }
+}
