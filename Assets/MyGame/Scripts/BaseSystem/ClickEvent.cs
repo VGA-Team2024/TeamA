80d8f70a9bb1d97bc2e138d89baa462c86d2e8eb
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-02-13 02:15:39 +0900

    [Update]クリック判定範囲の修正

--- Assets/MyGame/Scripts/BaseSystem/ClickEvent.cs
+++ Assets/MyGame/Scripts/BaseSystem/ClickEvent.cs
@@ -6,13 +6,36 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class ClickEvent : MonoBehaviour, IPointerClickHandler
 {
+    Vector3 _mousePos;
+    Vector3 _worldMousePos;
+    float _clickDistance = 0;
+    CircleCollider2D _circleColider;
+    Canvas _canvas = null;
+
+    void Start()
+    {
+        _canvas = transform.parent.GetComponent<Canvas>();
+        _circleColider = GetComponent<CircleCollider2D>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
+        _mousePos = eventData.position;
+        // キャンバスのz軸に合わせる
+        Vector3 screenMousePos = new Vector3(_mousePos.x, _mousePos.y, _canvas.transform.position.z);
+        // スクリーン座標からワールド座標に変換
+        _worldMousePos = Camera.main.ScreenToWorldPoint(screenMousePos);
+        _clickDistance = Vector2.Distance(_circleColider.transform.position, _worldMousePos);
         if (ResourceManager.Instance != null)
         {
+            // 左クリックのみ反応
             if (eventData.button == PointerEventData.InputButton.Left)
             {
-                ResourceManager.Instance.OnClick();
+                // 範囲判定
+                if (_clickDistance < _circleColider.bounds.extents.x / 1.07)
+                {
+                    ResourceManager.Instance.OnClick();
+                }
             }
         }
         else
