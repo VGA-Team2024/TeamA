39c3a888b6bebed008a7f8278395fe877f775c8f
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-02-14 07:54:27 +0900

    [Update]クリック判定範囲の計算方法の修正とそれに伴ったCanvasのRenderModeの修正

--- Assets/MyGame/Scripts/BaseSystem/ClickEvent.cs
+++ Assets/MyGame/Scripts/BaseSystem/ClickEvent.cs
@@ -6,33 +6,29 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class ClickEvent : MonoBehaviour, IPointerClickHandler
 {
-    Vector3 _mousePos;
-    Vector3 _worldMousePos;
-    float _clickDistance = 0;
-    CircleCollider2D _circleColider;
     Canvas _canvas = null;
+    RectTransform _rectTransform = null;
+    float _clickDistance = 0;
+    float _radius = 0;
 
     void Start()
     {
         _canvas = transform.parent.GetComponent<Canvas>();
-        _circleColider = GetComponent<CircleCollider2D>();
+        _rectTransform = GetComponent<RectTransform>();
+        _radius = _rectTransform.sizeDelta.x / 2 * _canvas.scaleFactor;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        _mousePos = eventData.position;
-        // キャンバスのz軸に合わせる
-        Vector3 screenMousePos = new Vector3(_mousePos.x, _mousePos.y, _canvas.transform.position.z);
-        // スクリーン座標からワールド座標に変換
-        _worldMousePos = Camera.main.ScreenToWorldPoint(screenMousePos);
-        _clickDistance = Vector2.Distance(_circleColider.transform.position, _worldMousePos);
+        _clickDistance = Vector2.Distance(_rectTransform.transform.position, eventData.position);
+        _radius = _rectTransform.sizeDelta.x / 2 * _canvas.scaleFactor;
         if (ResourceManager.Instance != null)
         {
             // 左クリックのみ反応
             if (eventData.button == PointerEventData.InputButton.Left)
             {
                 // 範囲判定
-                if (_clickDistance < _circleColider.bounds.extents.x / 1.07)
+                if (_clickDistance < _radius)
                 {
                     ResourceManager.Instance.OnClick();
                 }
