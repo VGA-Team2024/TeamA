f9b01af1f7890ae42693c9cbe6cd503409b36968
k.mitarai <kazmit299@gmail.com>
2024-12-18 00:26:50 +0900

    BuildStat調整

--- Assets/Scripts/BaseSystem/Dynamic/BuildState.cs
+++ Assets/Scripts/BaseSystem/Dynamic/BuildState.cs
@@ -1,8 +1,8 @@
+
 public class BuildState
 {
-    const string _hash = "test build";
-
-    public const string TeamID = "Foundation";
+    const string _hash = "b873373c-9280-4d57-9f9f-6bae57c95e9f";
+    public const string TeamID = "TeamA2024";
 
     public static string BuildHash
     {
