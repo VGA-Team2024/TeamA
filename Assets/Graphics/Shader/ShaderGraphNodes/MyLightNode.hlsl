aa0af7504757aa66af20b5a2567cf995deab4270
GotouTamaki <tamagoro0226@gmail.com>
2024-12-18 19:19:43 +0900

    [update]トゥーンシェーダー更新

--- Assets/Graphics/Shader/ShaderGraphNodes/MyLightNode.hlsl
+++ Assets/Graphics/Shader/ShaderGraphNodes/MyLightNode.hlsl
@@ -3,6 +3,7 @@
 
 #ifndef SHADERGRAPH_PREVIEW
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
 
 #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
 #pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
@@ -79,6 +80,66 @@ void GetReceiveShadow_float(float ShadowAlpha, float3 WorldPos, out half ShadowA
 
     ShadowAttenuation = shadow * ShadowAlpha;
 
+    #endif
+}
+
+#ifndef SHADERGRAPH_PREVIEW
+
+float4 TransformHClipToNormalizedScreenPos(float4 positionCS)
+{
+    float4 o = positionCS * 0.5f;
+    o.xy = float2(o.x, o.y * _ProjectionParams.x) + o.w;
+    o.zw = positionCS.zw;
+    return o / o.w;
+}
+
+half SampleOffsetDepth(float3 positionVS, float2 offset)
+{
+    // カメラとの距離やカメラのFOVで見た目上の輪郭の太さが変わらないように、オフセットをViewSpaceで計算する
+    float3 samplePositionVS = float3(positionVS.xy + offset, positionVS.z);
+    float4 samplePositionCS = TransformWViewToHClip(samplePositionVS);
+    float4 samplePositionVP = TransformHClipToNormalizedScreenPos(samplePositionCS);
+        
+    float offsetDepth = SAMPLE_TEXTURE2D_X(_CameraDepthTexture, sampler_CameraDepthTexture, samplePositionVP).r;
+    return offsetDepth;
+}
+
+#endif
+
+void SobelFilter_float(float3 PositionWS, float Thickness, float SobelFilterThreshold, out float4 Out)
+{
+    #ifdef SHADERGRAPH_PREVIEW
+    Out =  0.5;
+
+    #else
+    float3x3 sobel_x = float3x3(-1, 0, 1, -2, 0, 2, -1, 0, 1);
+    float3x3 sobel_y = float3x3(-1, -2, -1, 0, 0, 0, 1, 2, 1);
+
+    float edgeX = 0;
+    float edgeY = 0;
+
+    float3 positionVS = TransformWorldToView(PositionWS);
+
+    UNITY_UNROLL
+    for (int x = -1; x <= 1; x++)
+    {
+        UNITY_UNROLL
+        for (int y = -1; y <= 1; y++)
+        {
+            float2 offset = float2(x,y) * Thickness;
+            half depth = SampleOffsetDepth(positionVS, offset);
+            depth = LinearEyeDepth(depth, _ZBufferParams);
+                
+            float intensity = depth;
+            edgeX += intensity * sobel_x[x + 1][y + 1];
+            edgeY += intensity * sobel_y[x + 1][y + 1];
+        }
+    }
+
+    // エッジの強度を計算
+    float edgeStrength = length(float2(edgeX, edgeY));
+    edgeStrength = step(SobelFilterThreshold, edgeStrength);
+    Out = float4(edgeStrength, edgeStrength, edgeStrength, 1);
     #endif
 }
 
