959e3b5fd62a187f995ab988d8e8a2032f010326
GotouTamaki <tamagoro0226@gmail.com>
2025-02-14 00:21:15 +0900

    [update]プレイヤーが浮く問題解消、キャプチャ時の範囲表示(仮)

--- Assets/Scripts/Player/Capture/Capturaing.cs
+++ Assets/Scripts/Player/Capture/Capturaing.cs
@@ -6,5 +6,13 @@ using UnityEngine;
 
 public class Capturaing : OverlapDetectionSystem<Capturable>
 {
-   
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        Gizmos.matrix = Matrix4x4.TRS(transform.position + _offset, transform.rotation, transform.lossyScale);
+        Gizmos.DrawWireSphere(Vector3.zero, _radius);
+    }
+#endif
 }
